fdc8b5d28155d12cf6719f25df3229c4920d1f22
yannik1015 <34042618+yannik1015@users.noreply.github.com>
2023-06-26 13:15:23 +0200

    Implemented 4.2.16 SafeSyncCollection

--- weave-agent.sh
+++ weave-agent.sh
@@ -5,17 +5,17 @@
 # Generate .aj and .rmv files
 mkdir aj-files 2>/dev/null
 rm -r aj-files/* 2>/dev/null
-javamop -v -d aj-files mop-files/*   
+javamop -v -merge -d aj-files mop-files/*
+mv mop-files/*.rvm aj-files/   
 
 # Generate monitor classes from the .rvm files
 export CLASSPATH=$(pwd)/rv-monitor/target/release/rv-monitor/lib/rv-monitor-rt.jar:/usr/share/java/aspectjrt.jar:/usr/share/java/aspectjtools.jar
 mkdir -p aj-files/classes/mop
 cd aj-files
-rv-monitor -d classes/mop *.rvm 
+rv-monitor -merge -d classes/mop *.rvm 
 
 # Compile monitor classes
 javac classes/mop/*.java
-rm classes/mop/*.java
 
 # Generate the Java agent
 javamopagent *.aj classes -n JavaMopAgent -excludeJars
